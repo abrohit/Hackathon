31325d4c386e1bc2757d11b569fba049a3a355ea
Nakul Iyer <nakul@Nakuls-MacBook-Pro.local>
2020-08-14 14:56:48 -0400

    a

--- ClassRoom_Plus_Website/UserApi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from django.db import models
-from django.contrib.auth import get_user_model
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ('id','username', 'email')
